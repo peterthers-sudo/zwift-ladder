f5e175bb0db66a669f82143a0d99f7ed1c07728f
Peter <peterthers@gmail.com>
2026-04-03 03:57:25 +0100

    Auto update: 03-04-2026  3:57:25,70

--- data/fixtures.js
+++ data/fixtures.js
@@ -1,3 +1,3 @@
 // LEQP_FIXTURES_START
-const LEQP_FIXTURES = [{"date": "2026-04-02", "time": "17:30", "home": "LEQP Colombiere", "away": "SRT Plonkers", "route": "Downtown Titans", "powerups": []}, {"date": "2026-04-02", "time": "17:30", "home": "LEQP Aubisque", "away": "TFC Firebirds", "route": "Sand And Sequoias", "powerups": []}, {"date": "2026-04-02", "time": "18:30", "home": "LEQP Ventoux", "away": "Galaxy Avengers", "route": "BRAEk-fast Crits and Grits", "powerups": []}];
+const LEQP_FIXTURES = [{"date": "2026-04-09", "time": "17:45", "home": "LEQP Hautacam", "away": "Herd of Speedy Squirrels", "route": "", "powerups": []}];
 // LEQP_FIXTURES_END
