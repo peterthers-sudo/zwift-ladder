1c65fdb5d62492d0e535678a749fc1cc334c95dc
Peter <peterthers@gmail.com>
2026-04-04 14:02:13 +0100

    Auto update: 04-04-2026 14:02:13,05

--- data/fixtures.js
+++ data/fixtures.js
@@ -1,3 +1,3 @@
 // LEQP_FIXTURES_START
-const LEQP_FIXTURES = [{"date": "2026-04-09", "time": "17:30", "home": "SRT Storboskap", "away": "LEQP Ventoux", "route": "", "powerups": []}, {"date": "2026-04-09", "time": "17:30", "home": "SZR Delta Force", "away": "LEQP Colombiere", "route": "Coast to Coast", "powerups": []}, {"date": "2026-04-09", "time": "17:45", "home": "LEQP Hautacam", "away": "Herd of Speedy Squirrels", "route": "", "powerups": []}];
+const LEQP_FIXTURES = [{"date": "2026-04-09", "time": "17:30", "home": "SZR Delta Force", "away": "LEQP Colombiere", "route": "Coast to Coast", "powerups": []}, {"date": "2026-04-09", "time": "17:30", "home": "SRT Storboskap", "away": "LEQP Ventoux", "route": "", "powerups": []}, {"date": "2026-04-09", "time": "17:45", "home": "LEQP Hautacam", "away": "Herd of Speedy Squirrels", "route": "Innsbruckring", "powerups": []}];
 // LEQP_FIXTURES_END
