5b7443a0c9091fa265ccd72679cc4bb3778f1479
Peter <peterthers@gmail.com>
2026-04-11 03:11:07 +0100

    Auto update: 11-04-2026  3:11:07,81

--- data/fixtures.js
+++ data/fixtures.js
@@ -1,3 +1,3 @@
 // LEQP_FIXTURES_START
-const LEQP_FIXTURES = [{"date": "2026-04-13", "time": "17:30", "home": "LEQP Madeleine", "away": "WCC Axle Grinders", "route": "2018 Worlds Short Lap", "powerups": []}, {"date": "2026-04-16", "time": "17:30", "home": "Coalition Taraxyl", "away": "LEQP Aubisque", "route": "", "powerups": []}];
+const LEQP_FIXTURES = [{"date": "2026-04-13", "time": "17:30", "home": "LEQP Madeleine", "away": "WCC Axle Grinders", "route": "2018 Worlds Short Lap", "powerups": []}, {"date": "2026-04-16", "time": "17:25", "home": "Team Not Pogi Vuelta", "away": "LEQP Tourmalet", "route": "", "powerups": []}, {"date": "2026-04-16", "time": "17:30", "home": "Coalition Taraxyl", "away": "LEQP Aubisque", "route": "Downtown Eruption", "powerups": []}, {"date": "2026-04-16", "time": "17:30", "home": "SZR Phantom Force", "away": "LEQP Ventoux", "route": "", "powerups": []}];
 // LEQP_FIXTURES_END
