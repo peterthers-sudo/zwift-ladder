dbd56aa92f5e9875c7a7510d350e30b72c25d96d
Peter <peterthers@gmail.com>
2026-06-26 02:39:39 +0100

    Auto update: 26-06-2026  2:39:39,01

--- data/fixtures.js
+++ data/fixtures.js
@@ -1,3 +1,3 @@
 // LEQP_FIXTURES_START
-const LEQP_FIXTURES = [{"date": "2026-06-29", "time": "17:30", "home": "LEQP Colombiere", "away": "Cryo-Gen Derailleur Dynasty", "route": "Roule Ma Poule", "powerups": []}];
+const LEQP_FIXTURES = [{"date": "2026-06-29", "time": "17:30", "home": "LEQP Colombiere", "away": "Cryo-Gen Derailleur Dynasty", "route": "Roule Ma Poule", "powerups": []}, {"date": "2026-07-01", "time": "17:30", "home": "Herd of Tasmanian Devils", "away": "LEQP Madeleine", "route": "", "powerups": []}];
 // LEQP_FIXTURES_END
