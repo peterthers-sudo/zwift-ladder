80a91744d59bb5f7683771d80dfb93f7cdd1238c
Peter <peterthers@gmail.com>
2026-07-23 02:31:42 +0100

    Auto update: 23-07-2026  2:31:42,10

--- data/fixtures.js
+++ data/fixtures.js
@@ -1,3 +1,3 @@
 // LEQP_FIXTURES_START
-const LEQP_FIXTURES = [{"date": "2026-07-23", "time": "17:30", "home": "SRT Gold Diggers", "away": "LEQP Hautacam", "route": "R.G.V.", "powerups": []}];
+const LEQP_FIXTURES = [{"date": "2026-07-23", "time": "17:30", "home": "SRT Gold Diggers", "away": "LEQP Hautacam", "route": "R.G.V.", "powerups": []}, {"date": "2026-07-23", "time": "18:00", "home": "LEQP Tourmalet", "away": "RollCo Pumpernickel", "route": "Montmartre Mixer", "powerups": []}];
 // LEQP_FIXTURES_END
