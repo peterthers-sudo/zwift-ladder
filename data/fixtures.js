2e637be03fab190e389a3d2b468e2be6678210ff
peterthers-sudo <peterthers@gmail.com>
2026-09-18 02:37:17 +0200

    Auto update: 18-09-2026  2:37:17,76

--- data/fixtures.js
+++ data/fixtures.js
@@ -1,3 +1,3 @@
 // LEQP_FIXTURES_START
-const LEQP_FIXTURES = [{"date": "2026-09-17", "time": "17:30", "home": "Crazy Dogs", "away": "LEQP Aubisque", "route": "Seaside Sprint", "powerups": []}, {"date": "2026-09-17", "time": "17:30", "home": "LEQP Tourmalet", "away": "SZR Phantom Force", "route": "Loopin Lava", "powerups": []}];
+const LEQP_FIXTURES = [];
 // LEQP_FIXTURES_END
