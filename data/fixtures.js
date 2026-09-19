e8b3bb7138a8bebc45e3724bca3653c3165c27e8
peterthers-sudo <peterthers@gmail.com>
2026-09-19 02:16:15 +0200

    Auto update: 19-09-2026  2:16:15,13

--- data/fixtures.js
+++ data/fixtures.js
@@ -1,3 +1,3 @@
 // LEQP_FIXTURES_START
-const LEQP_FIXTURES = [];
+const LEQP_FIXTURES = [{"date": "2026-09-24", "time": "17:45", "home": "LEQP Hautacam", "away": "TBR Outcasts", "route": "Innsbruckring", "powerups": []}];
 // LEQP_FIXTURES_END
