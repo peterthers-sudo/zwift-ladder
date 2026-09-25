43429f68f33b757908056cb26e2cfad1151f4535
peterthers-sudo <peterthers@gmail.com>
2026-09-26 01:49:55 +0200

    Auto update: 26-09-2026  1:49:55,11

--- data/fixtures.js
+++ data/fixtures.js
@@ -1,3 +1,3 @@
 // LEQP_FIXTURES_START
-const LEQP_FIXTURES = [{"date": "2026-10-01", "time": "17:30", "home": "LEQP Madeleine", "away": "Coalition Taraxyl", "route": "", "powerups": []}, {"date": "2026-10-01", "time": "17:31", "home": "Galaxy Titans", "away": "LEQP Colombiere", "route": "", "powerups": []}];
+const LEQP_FIXTURES = [{"date": "2026-10-01", "time": "17:30", "home": "DIRTy Cycos", "away": "LEQP Hautacam", "route": "", "powerups": []}, {"date": "2026-10-01", "time": "17:30", "home": "LEQP Madeleine", "away": "Coalition Taraxyl", "route": "Sand And Sequoias", "powerups": []}, {"date": "2026-10-01", "time": "17:31", "home": "Galaxy Titans", "away": "LEQP Colombiere", "route": "R.G.V.", "powerups": []}];
 // LEQP_FIXTURES_END
